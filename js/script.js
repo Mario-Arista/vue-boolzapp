a0da077086f909751f37ad385916d7c34cd88d33
mario.arista <mario.arista.dev@gmail.com>
2024-02-24 18:52:58 +0100

    Changed Images and messages

--- js/script.js
+++ js/script.js
@@ -22,165 +22,210 @@ createApp({
 
             contacts: [
                 {
-                    name: 'Michele',
+                    name: 'Rag. Filini',
                     avatar: './img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Hai portato a spasso il cane?',
+                            date: '10/01/2024 15:30:55',
+                            message: 'Domani giochiamo a tennis?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Ricordati di stendere i panni',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 16:15:22',
-                            message: 'Tutto fatto!',
+                            date: '10/01/2024 15:50:00',
+                            message: "Va bene ragioniere, però o giochiamo la mattina all'alba o niente",
                             status: 'received'
-                        }
-                    ],
+                        },
+                        {
+                            date: '10/01/2024 16:15:22',
+                            message: "Ci sarà un po' freddino ma va bene...",
+                            status: 'sent'
+                        },
+                    ]
+
                 },
+
                 {
-                    name: 'Fabio',
+                    name: 'Megadirettore Galattico',
                     avatar: './img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
-                            message: 'Ciao come stai?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '20/03/2020 16:30:55',
-                            message: 'Bene grazie! Stasera ci vediamo?',
+                            date: '19/02/2024 16:30:00',
+                            message: 'Fantozzi, è convocato nel mio ufficio domani!',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
-                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            date: '19/02/2024 16:30:55',
+                            message: 'Per cosa Megadirettore? Se posso chiedere... umilmente...',
+                            status: 'sent'
+                        },
+                        {
+                            date: '19/02/2024 16:35:00',
+                            message: "Deve nuotare nel mio acquario umano!",
+                            status: 'received'
+                        },
+                        {
+                            date: '20/012/2024 15:30:55',
+                            message: "Ahhhhh com'è umano lei!",
                             status: 'sent'
                         }
-                    ],
+                    ]
                 },
+
                 {
-                    name: 'Samuele',
+                    name: 'Folagra',
                     avatar: './img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
-                            message: 'La Marianna va in campagna',
+                            date: '18/02/2024 10:10:40',
+                            message: 'Dobbiamo pensare ad una cogestione che sia proliferante in senso comune',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
-                            message: 'Sicuro di non aver sbagliato chat?',
+                            date: '18/02/2024 10:20:10',
+                            message: 'certo certo',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
-                            message: 'Ah scusa!',
+                            date: '18/02/2024 16:15:22',
+                            message: 'E allora cosa dobbiamo fare? È a monte che dobbiamo distruggere!!!',
+                            status: 'received'
+                        },
+                        {
+                            date: '18/02/2024 16:16:22',
+                            message: 'Hai capito?',
                             status: 'received'
                         }
-                    ],
+                    ]
                 },
                 {
-                    name: 'Alessandro B.',
+                    name: 'Sig.na Silvani',
                     avatar: './img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            date: '10/02/2024 15:30:55',
+                            message: 'Io sono stato azzurro di sci!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Si, ma preferirei andare al cinema',
+                            date: '10/02/2024 15:40:00',
+                            message: 'Eh?',
                             status: 'received'
-                        }
-                    ],
+                        },
+                        {
+                            date: '10/02/2024 15:50:55',
+                            message: 'Io sono stato nella nazionale di sci! Ma sono dieci anni che non scio',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/02/2024 15:50:58',
+                            message: 'Come?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/02/2024 16:50:55',
+                            message: 'Sto dicendo che saranno trenta, trentacinque anni che non vedo un paio di sci!',
+                            status: 'sent'
+                        },
+                    ]
                 },
                 {
-                    name: 'Alessandro L.',
+                    name: 'Pina',
                     avatar: './img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ricordati di chiamare la nonna',
+                            date: '20/02/2024 08:30:55',
+                            message: 'Preparami la frittatona di cipolle stasera!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Va bene, stasera la sento',
+                            date: '20/02/2024 08:31:55',
+                            message: 'Va bene!',
                             status: 'received'
-                        }
+                        },
+                        {
+                            date: '20/02/2024 08:35:55',
+                            message: 'Con la birra ghiacciata!',
+                            status: 'sent'
+                        },
                     ],
                 },
                 {
-                    name: 'Claudia',
+                    name: 'Mariangela',
                     avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ciao Claudia, hai novità?',
+                            date: '20/02/2020 08:15:55',
+                            message: 'Buongiorno scimmiett... Mariangela mia',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Non ancora',
+                            date: '20/02/2020 08:17:55',
+                            message: 'Ciao papi!',
                             status: 'received'
                         },
-                        {
-                            date: '10/01/2020 15:51:00',
-                            message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
-                        }
-                    ],
+                    ]
                 },
                 {
-                    name: 'Federico',
+                    name: 'Guidobaldo Maria Riccardelli',
                     avatar: './img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            date: '17/02/2024 15:30:55',
+                            message: 'Stasera è convocato per il Cineclub! Vedremo un capolavoro assoluto.. la Corazzata Kotiomkin!',
+                            status: 'received'
+                        },
+                        {
+                            date: '17/02/2024 15:36:55',
+                            message: "Ma stasera c'è Italia Inghilterra",
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            date: '17/02/2024 15:37:55',
+                            message: 'Non può mancare!',
                             status: 'received'
-                        }
-                    ],
+                        },
+                    ]
                 },
                 {
-                    name: 'Davide',
+                    name: 'Calboni',
                     avatar: './img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
-                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            message: 'Stasera prendiamo tre scotcs',
                             status: 'received'
                         },
                         {
                             date: '10/01/2020 15:50:00',
-                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            message: "Cos'è che beviamo, Calboni?",
                             status: 'sent'
                         },
                         {
                             date: '10/01/2020 15:51:00',
-                            message: 'OK!!',
+                            message: 'Tre scotcs!',
                             status: 'received'
                         }
-                    ],
+                    ]
+                },
+                {
+                    name: 'Maestro Canello',
+                    avatar: './img/avatar_9.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '31/12/2023 22:30:00',
+                            message: "Che capodano fantastico! Grazie maestro!",
+                            status: 'sent'
+                        }
+                    ]
                 }
             ]
             
@@ -249,6 +294,7 @@ createApp({
                 this.filteredContacts = this.contacts.filter(contact =>
                     contact.name.toLowerCase().includes(this.searchedString.toLowerCase())
                 );
+
 
             } else {
 
