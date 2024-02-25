8bb0a48088c33e48dc613edcf2898f837a401083
mario.arista <mario.arista.dev@gmail.com>
2024-02-25 20:22:29 +0100

    optimizing and starting milestone 5

--- js/script.js
+++ js/script.js
@@ -18,28 +18,33 @@ createApp({
             searchedString: "",
 
             // Array vuoto in cui mettere contatti da filtrare che parte da stringa
-            filteredContacts: "", 
+            filteredContacts: [],
+            
 
             contacts: [
                 {
                     name: 'Rag. Filini',
                     avatar: './img/avatar_1.jpg',
                     visible: true,
+                    random: "Batti lei?!",
                     messages: [
                         {
                             date: '10/01/2024 15:30:55',
                             message: 'Domani giochiamo a tennis?',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '10/01/2024 15:50:00',
                             message: "Va bene ragioniere, però o giochiamo la mattina all'alba o niente",
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '10/01/2024 16:15:22',
                             message: "Ci sarà un po' freddino ma va bene...",
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                     ]
 
@@ -49,26 +54,31 @@ createApp({
                     name: 'Megadirettore Galattico',
                     avatar: './img/avatar_2.jpg',
                     visible: true,
+                    random: "Fantozzi, è licenziato!",
                     messages: [
                         {
                             date: '19/02/2024 16:30:00',
                             message: 'Fantozzi, è convocato nel mio ufficio domani!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '19/02/2024 16:30:55',
                             message: 'Per cosa Megadirettore? Se posso chiedere... umilmente...',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '19/02/2024 16:35:00',
                             message: "Deve nuotare nel mio acquario umano!",
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '20/012/2024 15:30:55',
                             message: "Ahhhhh com'è umano lei!",
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         }
                     ]
                 },
@@ -77,26 +87,31 @@ createApp({
                     name: 'Folagra',
                     avatar: './img/avatar_3.jpg',
                     visible: true,
+                    random: "Viva la classe operaia!",
                     messages: [
                         {
                             date: '18/02/2024 10:10:40',
                             message: 'Dobbiamo pensare ad una cogestione che sia proliferante in senso comune',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '18/02/2024 10:20:10',
                             message: 'certo certo',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '18/02/2024 16:15:22',
                             message: 'E allora cosa dobbiamo fare? È a monte che dobbiamo distruggere!!!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '18/02/2024 16:16:22',
                             message: 'Hai capito?',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         }
                     ]
                 },
@@ -104,31 +119,37 @@ createApp({
                     name: 'Sig.na Silvani',
                     avatar: './img/avatar_4.jpg',
                     visible: true,
+                    random: "Fantocci",
                     messages: [
                         {
                             date: '10/02/2024 15:30:55',
                             message: 'Io sono stato azzurro di sci!',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '10/02/2024 15:40:00',
                             message: 'Eh?',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '10/02/2024 15:50:55',
                             message: 'Io sono stato nella nazionale di sci! Ma sono dieci anni che non scio',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '10/02/2024 15:50:58',
                             message: 'Come?',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '10/02/2024 16:50:55',
                             message: 'Sto dicendo che saranno trenta, trentacinque anni che non vedo un paio di sci!',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                     ]
                 },
@@ -136,21 +157,25 @@ createApp({
                     name: 'Pina',
                     avatar: './img/avatar_5.jpg',
                     visible: true,
+                    random: "Ugo, ma che ti è successo?",
                     messages: [
                         {
                             date: '20/02/2024 08:30:55',
                             message: 'Preparami la frittatona di cipolle stasera!',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '20/02/2024 08:31:55',
                             message: 'Va bene!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '20/02/2024 08:35:55',
                             message: 'Con la birra ghiacciata!',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                     ],
                 },
@@ -158,16 +183,19 @@ createApp({
                     name: 'Mariangela',
                     avatar: './img/avatar_6.jpg',
                     visible: true,
+                    random: "Papinoooooo",
                     messages: [
                         {
                             date: '20/02/2020 08:15:55',
                             message: 'Buongiorno scimmiett... Mariangela mia',
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '20/02/2020 08:17:55',
                             message: 'Ciao papi!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                     ]
                 },
@@ -175,21 +203,25 @@ createApp({
                     name: 'Guidobaldo Maria Riccardelli',
                     avatar: './img/avatar_7.jpg',
                     visible: true,
+                    random: "Che capolavoro la Corazzata Kotiomkin!",
                     messages: [
                         {
                             date: '17/02/2024 15:30:55',
                             message: 'Stasera è convocato per il Cineclub! Vedremo un capolavoro assoluto.. la Corazzata Kotiomkin!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '17/02/2024 15:36:55',
                             message: "Ma stasera c'è Italia Inghilterra",
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '17/02/2024 15:37:55',
                             message: 'Non può mancare!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                     ]
                 },
@@ -197,21 +229,25 @@ createApp({
                     name: 'Calboni',
                     avatar: './img/avatar_8.jpg',
                     visible: true,
+                    random: "Si dice scotcs",
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Stasera prendiamo tre scotcs',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: "Cos'è che beviamo, Calboni?",
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Tre scotcs!',
-                            status: 'received'
+                            status: 'received',
+                            showedFilter: false
                         }
                     ]
                 },
@@ -219,11 +255,13 @@ createApp({
                     name: 'Maestro Canello',
                     avatar: './img/avatar_9.jpg',
                     visible: true,
+                    random: "Vuole un autografo?",
                     messages: [
                         {
                             date: '31/12/2023 22:30:00',
                             message: "Che capodano fantastico! Grazie maestro!",
-                            status: 'sent'
+                            status: 'sent',
+                            showedFilter: false
                         }
                     ]
                 }
@@ -239,7 +277,7 @@ createApp({
     created() {
 
         // Mi copio tutti i contatti anche nei filtrati che all'inizio saranno gli stessi
-        this.filteredContacts = this.contacts;
+        this.filteredContacts = [...this.contacts];
 
     },
 
@@ -260,7 +298,8 @@ createApp({
                 this.contacts[this.indexForFriend].messages.push({
                     date: '23/02/2024 18:30:55',
                     message: this.myMessage,
-                    status: 'sent'
+                    status: 'sent',
+                    showedFilter: false,
                 });
 
                 // Pulisco l'input del messaggio dopo l'invio
@@ -278,8 +317,9 @@ createApp({
             // Aggiungo risposta casuale sia nella data che nel messaggio
             this.contacts[this.indexForFriend].messages.push({
                 date: '23/02/2024 18:30:56',
-                message: 'Bene, ma non benissimo!!!',
-                status: 'received'
+                message: this.contacts[this.indexForFriend].random,
+                status: 'received',
+                showedFilter: false
             });
 
         },
@@ -303,6 +343,18 @@ createApp({
 
             }
 
+
+        },
+
+        // Funzione per mostare pop up per cancellare messaggi
+        myFilter(messageIndex) {
+            
+            this.filteredContacts[this.indexForFriend].messages[messageIndex].showedFilter = !this.filteredContacts[this.indexForFriend].messages[messageIndex].showedFilter;
+            
+        },
+
+        // Funzione per cancellare messaggi
+        deleteMessage(messageIndex) {
 
         },
 
